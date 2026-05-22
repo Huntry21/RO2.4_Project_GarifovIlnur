2ec1b2c7a050e94b100c1da4efb56bd0ffd40f3c
Huntry <ilnurkz21134@gmail.com>
2026-05-22 14:03:11 +0500

    Stage 2: students list + detail navigation

---
+++ SchoolApp/StudentsPage.xaml.cs
@@ -0,0 +1,24 @@
+namespace SchoolApp;
+
+public partial class StudentsPage : ContentPage
+{
+    private readonly List<string> _students = new()
+    {
+        "Garifov Ilnur",
+        "Kurmashev Artur",
+        "Zhumabay Nurdaulet", 
+    };
+
+    public StudentsPage()
+    {
+        InitializeComponent();
+        StudentsCollection.ItemsSource = _students;
+    }
+
+    private async void OnStudentSelected(object sender, SelectionChangedEventArgs e)
+    {
+        if (e.CurrentSelection.FirstOrDefault() is not string name) return;
+        await Shell.Current.GoToAsync($"{nameof(StudentsDetailPage)}?name={Uri.EscapeDataString(name)}");
+        ((CollectionView)sender).SelectedItem = null;
+    }
+}
