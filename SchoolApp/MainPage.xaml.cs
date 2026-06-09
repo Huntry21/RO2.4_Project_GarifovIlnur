331c1747eca899d401e9be13cc09868e7fdf77d9
Huntry <ilnurkz21134@gmail.com>
2026-06-09 13:48:15 +0500

    L16: Person notifies the screen via INotifyPropertyChanged

--- SchoolApp/MainPage.xaml.cs
+++ SchoolApp/MainPage.xaml.cs
@@ -16,9 +16,15 @@ public partial class MainPage : ContentPage
     {
         _count++;
         StatusLabel.Text = $"Button tapped {_count} time(s).";
+
     }
     private async void OnOpenStudentsClicked(object sender, EventArgs e)
     {
         await Shell.Current.GoToAsync(nameof(StudentsPage));
+    }
+    private void OnShoutClicked(object sender, EventArgs e)
+    {
+        if (BindingContext is Person p)
+            p.Name = p.Name.ToUpper();
     }
 }
